101f122edace6727b29722938d88371aa81c7b7f
masson-maelys <maelys.masson@edu.esiee.fr>
2025-12-04 10:58:50 +0000

    exercice tuples

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #### Imports et définition des variables globales
-
+""" Module qui permet de retourner la liste de tuple d'une chaîne de caractères"""
 # Mandatory for the recursive solution to work on large inputs
 import sys
 sys.setrecursionlimit(2000)
@@ -9,7 +9,8 @@ sys.setrecursionlimit(2000)
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères passée en 
+    argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +18,25 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    liste = []
+    caractere = s[0]
+    compteur = 1
+    chaine = s[1:]
+    for i in chaine:
+        if i==caractere:
+            compteur += 1
+        else :
+            liste.append((caractere,compteur))
+            caractere = i
+            compteur = 1
 
-    return [ ]
+    liste.append((caractere,compteur))
+    return liste
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne
+     de caractères passée en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,20 +44,25 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
+    if not s:
+        return []
+    caractere = s[0]
+    compteur = 0
+    for i in s:
+        if i == caractere:
+            compteur += 1
+        else:
+            break
+    reste = artcode_r(s[compteur:])
+    return [(caractere, compteur)]+reste
 
 #### Fonction principale
 
 
 def main():
+    """
+    Docstring pour main
+    """
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
